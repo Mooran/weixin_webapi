bcdf849b61773763767a735e710dd3924c8b330c
mooran <mooran953@gmail.com>
2017-03-07 11:22:05 +0800

    处理微信返回字符串中包含特殊字符而无法json_decode问题

--- wx_listener.php
+++ wx_listener.php
@@ -251,6 +251,15 @@ class WebWeixin
 
         $data = $this->_post($url , array());
 
+        /* 处理微信返回字符串中包含特殊字符而无法decode问题 */
+        
+        $data = preg_replace(
+            '/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.
+            '|[\x00-\x7F][\x80-\xBF]+'.
+            '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'.
+            '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'.
+            '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S', '?', $data);
+
         $arr_data = json_decode($data, true);
 
         //file_put_contents('/tmp/data.json', $data);
